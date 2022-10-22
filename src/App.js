4ef4473167c203a22bc4f6bab7d6ec3579fae8ea
shafik720 <shafikrasel5@gmail.com>
2022-10-22 23:05:45 +0600

    sfddsg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Header from './Components/Header/Header';
+import Products from './Components/Products/Products';
 
 function App() {
   return (
     <div>
       <Header></Header>
+      <Products></Products>
     </div>
   );
 }
---
+++ src/Components/Products/Products.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Products.css'
+const Products = () => {
+    return (
+        <div className="product-page">
+            <div className="product-container">
+                <h2>Product Container</h2>
+            </div>
+            <div className="cart-container">
+                <h2>Cart Container</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
