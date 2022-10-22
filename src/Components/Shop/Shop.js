61fb188e1736e7663ca1b59ea4b9355e6038f5c5
shafik720 <shafikrasel5@gmail.com>
2022-10-22 23:40:01 +0600

    sfddsg

--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,10 +3,16 @@ import './Shop.css'
 
 const Shop = (props) => {
     console.log(props);
-    const{id, img, name, price, quantity} = props.index;
+    const{id, img, name, price, quantity, seller, ratings} = props.index;
     return (
         <div className="shop-card">
             <img src={img} alt=""/>
+            <div className="product-details">
+            <h4>Product Name : {name} </h4>
+            <p>Price : {price} </p>
+            <p>Seller : {seller} </p>
+            <p>Ratings : {ratings} </p>
+            </div>
         </div>
     );
 };
