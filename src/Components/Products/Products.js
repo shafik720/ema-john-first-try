ca5d1c4650e4d3a5365a6f46542b8f81de7a1703
shafik720 <shafikrasel5@gmail.com>
2022-10-22 23:10:01 +0600

    sfddsg

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Products.css'
 const Products = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data))
+    },[])
     return (
         <div className="product-page">
+            <h2>Total Product Found : {products.length} </h2>
             <div className="product-container">
-                <h2>Product Container</h2>
+                
             </div>
             <div className="cart-container">
                 <h2>Cart Container</h2>
