fdbd15fac85cdfa5ec81d87fdac7f695d5e02563
shafik720 <shafikrasel5@gmail.com>
2022-10-24 21:37:39 +0600

    dfgfdg

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -4,28 +4,29 @@ import './Cart.css'
 const Cart = (props) => {
     const {cart} = props;
 
+    // local  storage
+    let storeId = JSON.parse(localStorage.getItem('cart') || '[]');
+    storeId.forEach(index=>console.log(index));
+    // console.log(storeId);
+
     let tax = 0;
     let grandTotal = 0;
     let total = 0;
     let shipping = 0;
-    for(let element of cart){
+
+    for(let element of storeId){
         total += element.price;
         shipping += element.shipping;
     }
 
     tax = parseFloat(total * 0.1);
     grandTotal = tax + shipping + total;
-
-
-    // local  storage
-    let storeId = JSON.parse(localStorage.getItem('cart') || '[]');
-    storeId.forEach(index=>console.log(index));
-    // console.log(storeId);
+    
     return (
         <div className="cart-div">
             <div className="cart-child-div">
                 <h2>Order Summary </h2>
-                <p>Product Added : {cart.length} </p>
+                <p>Product Added : {storeId.length} </p>
                 <p>Total Price: {total} </p>
                 <p>Shipping Fee : {shipping} </p>
                 <p>Tax : {tax.toFixed(2)} </p>
