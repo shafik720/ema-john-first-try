21aa39911571c2fc71a9819e5f977cdc745500b4
shafik720 <shafikrasel5@gmail.com>
2022-10-24 14:38:36 +0600

    dfgfdg

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
+    const {cart} = props
+    console.log(cart)
     return (
         <div className="cart-div">
             <div className="cart-child-div">
                 <h2>Order Summary </h2>
-                <p>Product Added : {props.cart.length} </p>
+                <p>Product Added : {cart.length} </p>
             </div>
         </div>
     );
