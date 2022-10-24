3a3a9f5a1056b6d0b8a1a543607b9e85e7c61201
shafik720 <shafikrasel5@gmail.com>
2022-10-24 14:42:24 +0600

    dfgfdg

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,18 +2,23 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = (props) => {
-    const {cart} = props
-    console.log(cart)
+    const {cart} = props;
+    console.log(cart);
+
     let total = 0;
+    let shipping = 0;
     for(let element of cart){
         total += element.price;
+        shipping += element.shipping;
     }
+
     return (
         <div className="cart-div">
             <div className="cart-child-div">
                 <h2>Order Summary </h2>
                 <p>Product Added : {cart.length} </p>
                 <p>Total Price: {total} </p>
+                <p>Shipping Fee : {shipping} </p>
             </div>
         </div>
     );
